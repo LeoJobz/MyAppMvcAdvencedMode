ea853cd5312e29d968bea5fe2f1b45ad1e4e2af8
Leonardo Santos <leonardo.cienciacomputer@gmail.com>
2020-06-25 17:57:35 -0300

    Organizing the app structure

---
+++ src/DevIO.App/Configurations/IdentityConfig.cs
@@ -0,0 +1,27 @@
+﻿//using AutoMapper.Configuration;
+//using DevIO.App.Data;
+//using Microsoft.AspNetCore.Builder;
+//using Microsoft.AspNetCore.Http;
+//using Microsoft.EntityFrameworkCore;
+//using Microsoft.Extensions.DependencyInjection;
+
+//namespace DevIO.App.Configurations
+//{
+//    public static class IdentityConfig
+//    {
+//        public static IServiceCollection AddIdentityConfiguration(this IServiceCollection services, IConfiguration Configuration)
+//        {
+//            services.Configure<CookiePolicyOptions>(options =>
+//            {
+//                options.CheckConsentNeeded = context => true;
+//                options.MinimumSameSitePolicy = SameSiteMode.None;
+//            });
+
+//            services.AddDbContext<ApplicationDbContext>(options =>
+//                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+//            return services;
+//        }
+
+//    }
+//}
